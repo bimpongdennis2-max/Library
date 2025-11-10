c6ce315df4dd955033fe466d379715eb97b1286b
bimpongdennis2-max <bimpongdennis2@gmail.com>
2025-11-10 06:12:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,63 @@
-import streamlit as st
+print('Welcome to the Library')
+library_dictionary = {}
+while True :
+    print(f"1.Add book,\n2.Borrow book,\n3.Display All books,\n4.Exit")
+    g= input('Enter your choice: ')
+       
+    
+    def add_book(book_title, number_of_copies) : 
+       library_dictionary [book_title] = number_of_copies
+       return library_dictionary
+        
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    if g == '1' : 
+       book_title  = input ("Book title :")
+       number_of_copies = int(input ("Number of copies :"))
+       X = add_book(book_title , number_of_copies)
+
+       while True : 
+          Z = input('Do you want to input another book : ')
+          if Z == 'Yes' : 
+              book_title1 = input ("Book title :")
+              if book_title1 == book_title or book_title1 in library_dictionary : 
+                 new_number_of_copies = int(input('new_number_of_copies:'))
+                 library_dictionary[book_title1] = (new_number_of_copies + number_of_copies )
+           
+              else : 
+                 number_of_copies1 =int(input("Number of copies:"))
+                 y = add_book(book_title1, number_of_copies1)
+           
+          if Z == 'No' :
+              print(f"Thank you\nGoodbye.")
+              print(library_dictionary)             
+              break
+        
+        
+    if g == '2' : 
+       d = input('What book do you want to borrow: ')      
+       if d in library_dictionary : 
+          print(f"Book borrowed successfully")
+          for d in library_dictionary:
+             library_dictionary[d] -= 1
+             print(library_dictionary)
+       elif d in library_dictionary == 0 :
+             print('Book not available')
+                  
+       elif d != library_dictionary: 
+             print('Book not found')
+             
+    
+    if g == '3' :
+        for l in library_dictionary:
+            print(l)
+        if library_dictionary == {}: 
+            print('There are currently no books in the library ')       
+    if g == '4' : 
+        print('Goodbye.\nThank you for using the Library system')   
+        break
+        
+                                    
+            
+                 
+                 
+ 
